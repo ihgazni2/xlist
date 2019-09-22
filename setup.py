a7b05b2b7f363ad53965e05ae96bce7a1edac476
ihgazni2 <terryinzaghi@163.com>
2019-09-22 12:31:27 +0000

    remove old build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 
 setup(
       name="xlist",
-      version = "0.0.1", #@version@#
+      version = "0.0.2", #@version@#
       description="handle,.in progressing..,APIs",
       author="ihgazni2",
       url="https://github.com/ihgazni2/xlist",
@@ -37,4 +37,5 @@ setup(
 
 # python3 setup.py bdist --formats=tar
 # python3 setup.py sdist
+
 
